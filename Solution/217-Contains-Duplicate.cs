a563053d6aa3e57ed9b0008b7d589b589ce9b0d3
JayanthJaga <53475213+JayanthJaga@users.noreply.github.com>
2024-11-14 10:41:41 +0530

    [217] [Time Beats: 5.11%] [Memory Beats: 61.48%] - LeetPush

---
+++ Solution/217-Contains-Duplicate.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        //int nums =new int [] nums;
+        int nums_length = nums.Length;
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] == nums[j]) {
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
