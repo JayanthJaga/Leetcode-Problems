ad14a119e8797be3ac3ad4deeb0ecb353707c657
JayanthJaga <53475213+JayanthJaga@users.noreply.github.com>
2024-11-19 22:26:55 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 38.65%] - LeetPush

--- Solution/75-Sort-Colors.cs
+++ Solution/75-Sort-Colors.cs
@@ -1,29 +1,18 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        int n=nums.Length;
-        int count_zero=0,count_one=0,count_two=0;
-        for(int i=0;i<n;i++)
+        int large= 0;
+        for(int i=0;i<nums.Length;i++)
         {
-            if(nums[i]==0)
-            count_zero++;
-            else if(nums[i]==1)
-            count_one++;
-            else if(nums[i]==2)
-            count_two++;
-        }
-
-        for(int i =0;i<count_zero;i++)
-        {
-            nums[i]=0;
-        }
-        for(int i = count_zero;i<count_one+count_zero;i++)
-        {
-            nums[i]=1;
-        }
-        for(int i = count_zero+count_one;i<n;i++)
-        {
-            nums[i]=2;
-        }
+            for(int j=i+1;j<nums.Length;j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    large = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = large;
+                }
+            }
         
+        }
     }
 }
