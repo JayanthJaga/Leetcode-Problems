c58388597dadd0a6d7cd0a26aa6793d02bef2f1d
JayanthJaga <53475213+JayanthJaga@users.noreply.github.com>
2024-12-02 19:53:29 +0530

    [34] [Time Beats: 100.00%] [Memory Beats: 77.08%] - LeetPush

--- Solution/34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cs
+++ Solution/34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.cs
@@ -1,15 +1,37 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
-        int start =-1,end=-1;
-        for(int i=0;i<nums.Length;i++)
+            int first = SearchFirst(nums,target,true);
+            int last = SearchFirst(nums,target,false);
+
+            return new[] {first,last};
+
+
+    }
+
+    public static int SearchFirst(int[] nums,int target,bool first)
+    {
+        int left =0;
+        int right = nums.Length-1;
+        int result =-1;
+        
+        while(left<=right)
         {
-            if(nums[i]==target){
-                if(start==-1){
-                    start=i;
-                }
-                end=i;
+            int mid = left+(right-left)/2;
+            int midVal=nums[mid];
+            if(midVal==target){
+                result=mid;
+                if(first==true)
+                    right=mid-1;
+                else
+                    left=mid+1;
             }
+            else if(midVal>target){
+                right=mid-1;
+            }
+            else 
+                left=mid+1;
         }
-        return new[] {start,end };
+
+        return result;
     }
 }
